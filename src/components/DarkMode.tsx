3d043a2dcb65553e8758bf80e62e5804d1be3be4
AyubxonUz <yuldoshevv7@gmail.com>
2024-04-03 02:13:43 +0500

    Bugs have been fixed

--- src/components/DarkMode.tsx
+++ src/components/DarkMode.tsx
@@ -1,4 +1,5 @@
 import {useEffect, useState} from "react"
+import {toast} from "sonner"
 
 function DarkMode() {
   const getThemeFromLocalStorage = () => {
@@ -17,7 +18,22 @@ function DarkMode() {
   }, [theme])
   return (
     <>
-      {navigator.appCodeName !== "Safari" ? (
+      {navigator.userAgent.indexOf("Safari") > -1 ? (
+        <button
+          onClick={() => {
+            toast.error("Light mode is not supported in Safari")
+          }}
+          className="btn btn-circle btn-ghost"
+        >
+          <svg
+            className="swap-on fill-current w-[22px] h-[22px]"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+          >
+            <path d="M21.64,13a1,1,0,0,0-1.05-.14,8.05,8.05,0,0,1-3.37.73A8.15,8.15,0,0,1,9.08,5.49a8.59,8.59,0,0,1,.25-2A1,1,0,0,0,8,2.36,10.14,10.14,0,1,0,22,14.05,1,1,0,0,0,21.64,13Zm-9.5,6.69A8.14,8.14,0,0,1,7.08,5.22v.27A10.15,10.15,0,0,0,17.22,15.63a9.79,9.79,0,0,0,2.1-.22A8.11,8.11,0,0,1,12.14,19.73Z" />
+          </svg>
+        </button>
+      ) : (
         <label className="swap shadow-none swap-rotate btn btn-circle">
           {/* this hidden checkbox controls the state */}
           <input
@@ -45,8 +61,6 @@ function DarkMode() {
             <path d="M21.64,13a1,1,0,0,0-1.05-.14,8.05,8.05,0,0,1-3.37.73A8.15,8.15,0,0,1,9.08,5.49a8.59,8.59,0,0,1,.25-2A1,1,0,0,0,8,2.36,10.14,10.14,0,1,0,22,14.05,1,1,0,0,0,21.64,13Zm-9.5,6.69A8.14,8.14,0,0,1,7.08,5.22v.27A10.15,10.15,0,0,0,17.22,15.63a9.79,9.79,0,0,0,2.1-.22A8.11,8.11,0,0,1,12.14,19.73Z" />
           </svg>
         </label>
-      ) : (
-        <h1>Safari</h1>
       )}
     </>
   )
